d51be36455a9634efd3bbae9b5116b8d87b06827
Jmsmr <Jmsmr@users.noreply.github.com>
2023-05-08 00:37:50 +0800

    Updated Branch of me

--- LibraryManagementSystemWF/views/SignIn.Designer.cs
+++ LibraryManagementSystemWF/views/SignIn.Designer.cs
@@ -29,176 +29,138 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignIn));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtUsername = new System.Windows.Forms.TextBox();
-            this.txtPassword = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            this.SuspendLayout();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            pictureBox2 = new PictureBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(302, 6);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(123, 96);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(302, 6);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(123, 96);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(315, 108);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 32);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "LOG IN";
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(315, 108);
+            label1.Name = "label1";
+            label1.Size = new Size(96, 32);
+            label1.TabIndex = 1;
+            label1.Text = "LOG IN";
             // 
             // txtUsername
             // 
-            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtUsername.Location = new System.Drawing.Point(211, 179);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.PlaceholderText = "USERNAME";
-            this.txtUsername.Size = new System.Drawing.Size(291, 33);
-            this.txtUsername.TabIndex = 3;
-            this.txtUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            txtUsername.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            txtUsername.Location = new Point(211, 179);
+            txtUsername.Name = "txtUsername";
+            txtUsername.PlaceholderText = "USERNAME";
+            txtUsername.Size = new Size(291, 33);
+            txtUsername.TabIndex = 3;
+            txtUsername.TextAlign = HorizontalAlignment.Center;
             // 
             // txtPassword
             // 
-            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtPassword.Location = new System.Drawing.Point(211, 234);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.PlaceholderText = "PASSWORD";
-            this.txtPassword.Size = new System.Drawing.Size(291, 33);
-            this.txtPassword.TabIndex = 4;
-            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            txtPassword.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            txtPassword.Location = new Point(211, 234);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PlaceholderText = "PASSWORD";
+            txtPassword.Size = new Size(291, 33);
+            txtPassword.TabIndex = 4;
+            txtPassword.TextAlign = HorizontalAlignment.Center;
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.DimGray;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.Location = new System.Drawing.Point(368, 283);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(134, 34);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "REGISTER";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            button1.BackColor = Color.DimGray;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(192, 255, 255);
+            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(192, 255, 255);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(368, 283);
+            button1.Name = "button1";
+            button1.Size = new Size(134, 34);
+            button1.TabIndex = 10;
+            button1.Text = "REGISTER";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click_1;
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.Color.DimGray;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button2.Location = new System.Drawing.Point(211, 283);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(134, 34);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "LOG IN";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            button2.BackColor = Color.DimGray;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseDownBackColor = Color.FromArgb(192, 255, 255);
+            button2.FlatAppearance.MouseOverBackColor = Color.FromArgb(192, 255, 255);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(211, 283);
+            button2.Name = "button2";
+            button2.Size = new Size(134, 34);
+            button2.TabIndex = 11;
+            button2.Text = "LOG IN";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click_1;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(-2, -15);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(197, 345);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 12;
-            this.pictureBox2.TabStop = false;
+            pictureBox2.BackColor = Color.FromArgb(255, 192, 128);
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(-2, -3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(197, 333);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 12;
+            pictureBox2.TabStop = false;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(241, 150);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(245, 17);
-            this.label2.TabIndex = 13;
-            this.label2.Text = "Welcome! Please login to your account";
-            // 
-            // panel1
-            // 
-            panel1.BackColor = Color.LightGray;
-            panel1.Controls.Add(btnMini);
-            panel1.Controls.Add(btnClose);
-            panel1.Dock = DockStyle.Top;
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(519, 23);
-            panel1.TabIndex = 14;
-            // 
-            // btnMini
-            // 
-            btnMini.BackColor = Color.LightGray;
-            btnMini.BackgroundImage = (Image)resources.GetObject("btnMini.BackgroundImage");
-            btnMini.BackgroundImageLayout = ImageLayout.Zoom;
-            btnMini.Dock = DockStyle.Right;
-            btnMini.Location = new Point(459, 0);
-            btnMini.Name = "btnMini";
-            btnMini.Size = new Size(30, 23);
-            btnMini.TabIndex = 2;
-            btnMini.UseVisualStyleBackColor = false;
-            btnMini.Click += button5_Click;
-            // 
-            // btnClose
-            // 
-            btnClose.BackColor = Color.LightGray;
-            btnClose.BackgroundImage = (Image)resources.GetObject("btnClose.BackgroundImage");
-            btnClose.BackgroundImageLayout = ImageLayout.Zoom;
-            btnClose.Dock = DockStyle.Right;
-            btnClose.Location = new Point(489, 0);
-            btnClose.Name = "btnClose";
-            btnClose.Size = new Size(30, 23);
-            btnClose.TabIndex = 0;
-            btnClose.UseVisualStyleBackColor = false;
-            btnClose.Click += button3_Click;
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(241, 150);
+            label2.Name = "label2";
+            label2.Size = new Size(245, 17);
+            label2.TabIndex = 13;
+            label2.Text = "Welcome! Please login to your account";
             // 
             // SignIn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(519, 329);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.txtUsername);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "SignIn";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "SignIn";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            BackgroundImageLayout = ImageLayout.None;
+            ClientSize = new Size(519, 329);
+            Controls.Add(label2);
+            Controls.Add(pictureBox2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "SignIn";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SignIn";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
