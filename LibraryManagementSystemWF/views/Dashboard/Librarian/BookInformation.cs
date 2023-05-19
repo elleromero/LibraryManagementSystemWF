6f4c6bcf75e70c4b5d6e707f270de3c747755fe3
omineko <121761913+omineko@users.noreply.github.com>
2023-05-19 23:06:16 +0800

    added book information

---
+++ LibraryManagementSystemWF/views/Dashboard/Librarian/BookInformation.cs
@@ -0,0 +1,54 @@
+﻿using LibraryManagementSystemWF.models;
+using LibraryManagementSystemWF.utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryManagementSystemWF.views.Dashboard.Librarian
+{
+    public partial class BookInformation : Form
+    {
+        private Book book = new();
+
+        public BookInformation(Book book)
+        {
+            InitializeComponent();
+
+            this.book = book;
+
+            // initialize information
+            if (File.Exists(book.Cover))
+            {
+                coverPictureBox.Image = Image.FromFile(book.Cover);
+                bookCoverPictureBox.Image = Image.FromFile(book.Cover);
+            }
+
+            if (string.IsNullOrWhiteSpace(book.Genre.Name))
+            {
+                genreLbl.Text = "NO GENRE";
+            }
+            else
+            {
+                genreLbl.Text = book.Genre.Name;
+                genreLbl.BackColor = ColorTranslator.FromHtml(PastelColorGenerator.GeneratePastelColor(book.Genre.Name));
+            }
+            titleLbl.Text = book.Title;
+            authorLbl.Text = $"by {book.Author}";
+            datePublishedLbl.Text = $"Published on {book.PublicationDate.ToString("MMMM d, yyyy")}";
+            txtSypnosis.Text = string.IsNullOrWhiteSpace(book.Sypnosis) ? "No sypnosis available" : book.Sypnosis;
+            publisherLbl.Text = book.Publisher;
+            isbnLbl.Text = book.ISBN;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
