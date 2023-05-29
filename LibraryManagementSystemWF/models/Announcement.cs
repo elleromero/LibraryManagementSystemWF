c840def928c2c044bfc7089e0d3c9facf24ba34a
omineko <121761913+omineko@users.noreply.github.com>
2023-05-29 21:07:43 +0800

    added announcement model

---
+++ LibraryManagementSystemWF/models/Announcement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystemWF.models
+{
+    internal class Announcement
+    {
+        public Guid ID { get; set; }
+
+        public string Header { get; set; } = string.Empty;
+
+        public string Body { get; set; } = string.Empty;
+
+        public DateTime Due { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public string Cover { get; set; } = string.Empty;
+
+        public bool IsPriority { get; set; }
+
+        public User User { get; set; } = new();
+    }
+}
