3d451a3ee658c9353bda8647b3a4a67bc3169833
omineko <121761913+omineko@users.noreply.github.com>
2023-05-29 21:08:36 +0800

    updated announcement designs

---
+++ LibraryManagementSystemWF/views/Dashboard/Admin/AdminAnnouncement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryManagementSystemWF.views.Dashboard.Admin
+{
+    public partial class AdminAnnouncement : Form
+    {
+        public AdminAnnouncement()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
