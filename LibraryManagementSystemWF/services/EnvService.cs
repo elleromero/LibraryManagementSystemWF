83d5c2776f40e8998c84e1d9385ebdcfd74b86f4
omineko <121761913+omineko@users.noreply.github.com>
2023-06-15 00:33:12 +0800

    updated version

--- LibraryManagementSystemWF/services/EnvService.cs
+++ LibraryManagementSystemWF/services/EnvService.cs
@@ -15,7 +15,7 @@ namespace LibraryManagementSystemWF.services
             DB_NAME = "DB_LMS",
             CONN_BASE = "Data Source = DESKTOP-PDLDGGJ\\SQLEXPRESS;Integrated Security=true;",
             CONN_STR = $"{CONN_BASE}Initial Catalog={DB_NAME}",
-            VERSION = "3.2.0 Marmalade - debug branch";
+            VERSION = "3.3.0 Marmalade - debug branch";
         private const int BORROW_TIME_DAYS = 14;
                            
         public static int GetBorrowTimeDays()
