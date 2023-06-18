64bed22aeccf54d94d713135035d9cbeab3270d9
Elle <121761913+omineko@users.noreply.github.com>
2023-06-18 15:51:23 +0800

    Update EnvService.cs

--- LibraryManagementSystemWF/services/EnvService.cs
+++ LibraryManagementSystemWF/services/EnvService.cs
@@ -15,7 +15,7 @@ namespace LibraryManagementSystemWF.services
             DB_NAME = "DB_LMS",
             CONN_BASE = "Data Source = DESKTOP-PDLDGGJ\\SQLEXPRESS;Integrated Security=true;",
             CONN_STR = $"{CONN_BASE}Initial Catalog={DB_NAME}",
-            VERSION = "v3.9.6 - stable branch";
+            VERSION = "v4.0 - stable branch";
         private const int BORROW_TIME_DAYS = 7;
                            
         public static int GetBorrowTimeDays()
